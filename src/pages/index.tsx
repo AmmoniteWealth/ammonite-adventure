a4987c3ca63fe5e96dc4b49f0b1b9b8a4caef1a0
Laura <laura@ammonitewealth.com>
2023-03-31 15:00:29 +0100

    removing config for Netlify build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,40 +2,40 @@ import Link from "next/link";
 import Layout from "../components/layout";
 import Header from "@/components/header";
 import React from "react";
-import { getIsClient } from "./api/getIsClient";
-import {
-  useGetTenantConfigQuery,
-  useGetNonClientDataQuery,
-} from "../Functions/config";
+// import { getIsClient } from "./api/getIsClient";
+// import {
+//   useGetTenantConfigQuery,
+//   useGetNonClientDataQuery,
+// } from "../Functions/config";
 
 export default function Home() {
-  const hostname =
-    (typeof window !== "undefined" && window.location.hostname) || "localhost";
+  // const hostname =
+  //   (typeof window !== "undefined" && window.location.hostname) || "localhost";
 
-  const { data: tenantConfig, isFetching: isFetchingTenantConfig } =
-    useGetTenantConfigQuery(hostname, {
-      skip: false,
-      pollingInterval: 0,
-      refetchOnMountOrArgChange: false,
-      refetchOnFocus: false,
-      refetchOnReconnect: false,
-    });
+  // const { data: tenantConfig, isFetching: isFetchingTenantConfig } =
+  //   useGetTenantConfigQuery(hostname, {
+  //     skip: false,
+  //     pollingInterval: 0,
+  //     refetchOnMountOrArgChange: false,
+  //     refetchOnFocus: false,
+  //     refetchOnReconnect: false,
+  //   });
 
-  const { data: nonClientData, isFetching: isFetchingNonClientData } =
-    useGetNonClientDataQuery(hostname);
+  // const { data: nonClientData, isFetching: isFetchingNonClientData } =
+  //   useGetNonClientDataQuery(hostname);
 
-  if (isFetchingTenantConfig || isFetchingNonClientData)
-    return <div>Loading...</div>;
+  // if (isFetchingTenantConfig || isFetchingNonClientData)
+  //   return <div>Loading...</div>;
 
-  if (!tenantConfig || !nonClientData) return <div>Missing config!</div>;
+  // if (!tenantConfig || !nonClientData) return <div>Missing config!</div>;
 
-  console.log("DATA", tenantConfig.data.config.About.Card1.intro.default);
-  const _config = tenantConfig.data.config;
-  console.log("_config", _config);
+  // console.log("DATA", tenantConfig.data.config.About.Card1.intro.default);
+  // const _config = tenantConfig.data.config;
+  // console.log("_config", _config);
 
   return (
     <Layout story={undefined}>
-      <Header title={getIsClient(_config.About.Card1.intro)} zoom={undefined} />
+      <Header title={"Hello"} zoom={undefined} />
       <br />
       <Link href="/s/start">Start</Link>
       <Link href="/makeYourOwn">Add your details</Link>
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -10,32 +10,32 @@ import {
 } from "../Functions/config";
 export default function CustomSuccess() {
   const { user } = useAppContext();
-  const hostname =
-    (typeof window !== "undefined" && window.location.hostname) || "localhost";
+  // const hostname =
+  //   (typeof window !== "undefined" && window.location.hostname) || "localhost";
 
-  const { data: tenantConfig, isFetching: isFetchingTenantConfig } =
-    useGetTenantConfigQuery(hostname, {
-      skip: false,
-      pollingInterval: 0,
-      refetchOnMountOrArgChange: false,
-      refetchOnFocus: false,
-      refetchOnReconnect: false,
-    });
+  // const { data: tenantConfig, isFetching: isFetchingTenantConfig } =
+  //   useGetTenantConfigQuery(hostname, {
+  //     skip: false,
+  //     pollingInterval: 0,
+  //     refetchOnMountOrArgChange: false,
+  //     refetchOnFocus: false,
+  //     refetchOnReconnect: false,
+  //   });
 
-  const { data: nonClientData, isFetching: isFetchingNonClientData } =
-    useGetNonClientDataQuery(hostname);
+  // const { data: nonClientData, isFetching: isFetchingNonClientData } =
+  //   useGetNonClientDataQuery(hostname);
 
-  if (isFetchingTenantConfig || isFetchingNonClientData)
-    return <div>Loading...</div>;
+  // if (isFetchingTenantConfig || isFetchingNonClientData)
+  //   return <div>Loading...</div>;
 
-  if (!tenantConfig || !nonClientData) return <div>Missing config!</div>;
+  // if (!tenantConfig || !nonClientData) return <div>Missing config!</div>;
 
-  console.log(
-    "DATA from success",
-    tenantConfig.data.config.About.Card1.intro.default
-  );
-  const _config = tenantConfig.data.config;
-  console.log("_config from success", _config);
+  // console.log(
+  //   "DATA from success",
+  //   tenantConfig.data.config.About.Card1.intro.default
+  // );
+  // const _config = tenantConfig.data.config;
+  // console.log("_config from success", _config);
 
   return (
     <>
@@ -46,7 +46,7 @@ export default function CustomSuccess() {
           zoom={undefined}
         />
         <br />
-        <section>{getIsClient(_config.AppSettings.tenantFCAtext)}</section>
+        <section>{"Some other text"}</section>
         <Link href="/">Go home</Link>
       </Layout>
     </>
